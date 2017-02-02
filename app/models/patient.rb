8fe48d14323bdeaabd174bc31c61d4449a5d8032
musaomondi <amokem@yahoo.com>
2017-02-02 16:24:41 +0300

    validating presence

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,2 +1,7 @@
 class Patient < ApplicationRecord
+  validates :name, presence: true
+  validates :age, presence: true
+  validates :gender, presence: true
+  validates :town, presence: true
+  validates :county, presence: true
 end
