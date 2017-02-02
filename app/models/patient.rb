a33a926471862294d8cbe258d576c852da90baa4
musaomondi <amokem@yahoo.com>
2017-02-02 16:30:49 +0300

    adding a length validation for the name,town and county attributes

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,7 +1,7 @@
 class Patient < ApplicationRecord
-  validates :name, presence: true
+  validates :name, presence: true, length: { maximum: 50  }
   validates :age, presence: true
   validates :gender, presence: true
-  validates :town, presence: true
-  validates :county, presence: true
+  validates :town, presence: true, length: { maximum: 20  }
+  validates :county, presence: true, length: { maximum: 20  }
 end
