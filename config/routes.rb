62b5715e4fefe77426fcb8e9abe4beecc4e6a4f4
musaomondi <amokem@yahoo.com>
2017-02-05 22:55:48 +0300

    adding a resource to get the standard RESTful actions for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
 
   get 'contact' => 'static_pages#contact'
   get 'about' => 'static_pages#about'
+  get'login'=> 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 
   resources :patients
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
