f28b87ed583c521e0527c5f84840878dfcb223e1
musaomondi <amokem@yahoo.com>
2017-02-07 17:54:35 +0300

    validating the uniqueness of email addresses

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
   validates :first_name, presence: true, length: { maximum: 30  }
   validates :last_name, presence: true, length: { maximum: 30 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255  }, format: { with: VALID_EMAIL_REGEX  }
+  validates :email, presence: true, length: { maximum: 255  },
+                    format: { with: VALID_EMAIL_REGEX  },
+                    uniqueness: true
 end
