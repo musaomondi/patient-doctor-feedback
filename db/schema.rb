f0f6efcfcb1f2ee3168e6ce0c2f2d78020c4d7b4
musaomondi <amokem@yahoo.com>
2017-02-07 18:27:14 +0300

    migration to add a password_digest column to the users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170207150744) do
+ActiveRecord::Schema.define(version: 20170207152208) do
 
   create_table "patients", force: :cascade do |t|
     t.string   "name"
@@ -29,8 +29,9 @@ ActiveRecord::Schema.define(version: 20170207150744) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
