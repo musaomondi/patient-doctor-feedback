4ac6f71df0008701f0d05f3f90c79d90e716a9b2
musaomondi <amokem@yahoo.com>
2017-02-07 17:47:14 +0300

    migration to add users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170202134318) do
+ActiveRecord::Schema.define(version: 20170207144549) do
 
   create_table "patients", force: :cascade do |t|
     t.string   "name"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 20170202134318) do
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
     t.string   "phone_number"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
