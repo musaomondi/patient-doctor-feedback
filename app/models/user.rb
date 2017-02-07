60f8b212d58836e1bc272a997ce6ad95c8a98f3f
musaomondi <amokem@yahoo.com>
2017-02-07 17:51:02 +0300

    validating presence and length

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :first_name, presence: true, length: { maximum: 30  }
+  validates :last_name, presence: true, length: { maximum: 30 }
+  validates :email, presence: true, length: { maximum: 255  }
 end
