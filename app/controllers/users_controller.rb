2973a3fc4ebd19841da1de0de92c5648167da36f
musaomondi <amokem@yahoo.com>
2017-02-09 16:37:18 +0300

    logging in upon signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,6 +30,9 @@ class UsersController < ApplicationController
       if @user.save
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
+        log_in @user
+        flash[:success] = "Welcome to ...."
+        redirect_to @user
       else
         format.html { render :new }
         format.json { render json: @user.errors, status: :unprocessable_entity }
