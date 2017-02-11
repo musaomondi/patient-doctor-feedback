e5cf043e9a321d7bdc71e3ef0bd7753f55de074b
musaomondi <amokem@yahoo.com>
2017-02-11 22:23:26 +0300

    creating a relationship btn user patient and comment_type

--- app/models/user_patient.rb
+++ app/models/user_patient.rb
@@ -1,2 +1,7 @@
 class UserPatient < ApplicationRecord
+  include ActionView::Helpers::TextHelper
+    attr_accessible :amount, :comment_type_id, :comments, :patient_id, :user_id, :archive
+belongs_to :user
+belongs_to :patient
+belongs_to :comment_type, :foreign_key => :comment_type_id
 end
