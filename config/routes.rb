2e783fd837b891a919a10a5042b3d74cf42311f6
musaomondi <amokem@yahoo.com>
2017-02-13 17:26:17 +0300

    updating routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   resources :users
-  get 'sessions/new'
-
   root 'static_pages#home'
-
   get 'contact' => 'static_pages#contact'
   get 'about' => 'static_pages#about'
   get'login'=> 'sessions#new'
@@ -20,5 +17,14 @@ Rails.application.routes.draw do
       end
     end
   end
+
+  get 'reports' => "reports#reports"
+  get 'my_reports' => "reports#my_reports"
+  get 'fetchReports' => "reports#fetch_reports"
+  get 'fetch_my_reports' => "reports#fetch_my_reports"
+  get 'fetchReportsPdf' => "reports#fetch_reports_download"
+  get 'searchPatients' => 'patients#searchPatients'
+  get 'search' => 'patients#search'
+  get 'downloadPdf' => 'patients#downloadPdf'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
