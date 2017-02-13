33fa339e5c582afe907427e224f9863c825fc4e9
musaomondi <amokem@yahoo.com>
2017-02-13 17:53:24 +0300

    private method to pre-populate different types of users such as ADMIN, DOCTOR, STAFF

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,7 @@ class UsersController < ApplicationController
 
   # GET /users/1/edit
   def edit
+    getUserTypes
   end
 
   # POST /users
@@ -40,6 +41,7 @@ class UsersController < ApplicationController
         flash[:success] = "Welcome to ...."
         redirect_to user
       else
+        getUserTypes
         format.html { render :new }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
@@ -54,6 +56,7 @@ class UsersController < ApplicationController
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
         format.json { render :show, status: :ok, location: @user }
       else
+        getUserTypes
         format.html { render :edit }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
@@ -71,13 +74,16 @@ class UsersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
+  def getUserTypes
+    @user_types = UserType.all.map {|ut| [ut.user_type_name, ut.id]}
+  end
 end
