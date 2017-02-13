51627d97c2694f7e9576d1e4623371312e6381e9
musaomondi <amokem@yahoo.com>
2017-02-13 18:50:07 +0300

    validations

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,7 +1,23 @@
 class Patient < ApplicationRecord
+  has_many :user_patients
+  has_many :users, :through => :user_patients
+
   validates :name, presence: true, length: { maximum: 50  }
   validates :age, presence: true
   validates :gender, presence: true
   validates :town, presence: true, length: { maximum: 20  }
   validates :county, presence: true, length: { maximum: 20  }
+  validates :phone_number, presence: true
+
+  def date_of_birth_string
+    if !date_of_birth.blank? && date_of_birth != nil
+      date_of_birth.strftime("%d-%m-%Y")
+    end
+  end
+
+  def created_at_string
+    if !created_at.blank? && created_at != nil
+      created_at.strftime("%d-%m-%Y")
+    end
+  end
 end
--- app/models/user_patient.rb
+++ app/models/user_patient.rb
@@ -4,4 +4,24 @@ class UserPatient < ApplicationRecord
   belongs_to :user
   belongs_to :patient
   belongs_to :comment_type, :foreign_key => :comment_type_id
+
+  def created_at_string
+    if !created_at.blank? && created_at != nil
+      created_at.strftime("%d-%m-%Y")
+    end
+  end
+
+  def comments_html
+    if !created_at.blank? && created_at != nil
+      simple_format(comments)
+    end
+  end
+
+  def isArchive?
+    if archive == SessionsHelper::ARCHIVE
+      return true
+    else
+      return false
+    end
+  end
 end
