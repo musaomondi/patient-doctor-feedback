b9ce0562cb5b92440f88255b421b79eb7b30d324
musaomondi <amokem@yahoo.com>
2017-02-13 17:36:43 +0300

    action to render search page to enable patient search feature

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -63,6 +63,8 @@ class PatientsController < ApplicationController
       format.json { head :no_content }
     end
   end
+  def search
+  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
