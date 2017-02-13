2c1f67439bfbf448bf96ff92d3d9c1d0114ce53d
musaomondi <amokem@yahoo.com>
2017-02-13 16:02:45 +0300

    migration to add user_type to the db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170211191640) do
+ActiveRecord::Schema.define(version: 20170213130133) do
 
   create_table "patients", force: :cascade do |t|
     t.string   "name"
@@ -34,6 +34,12 @@ ActiveRecord::Schema.define(version: 20170211191640) do
     t.string   "archive"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+  end
+
+  create_table "user_types", force: :cascade do |t|
+    t.string   "user_type_name"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
   end
 
   create_table "users", force: :cascade do |t|
